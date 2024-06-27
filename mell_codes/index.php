47827ee390fe0562f443aa776c9a8efb943cc8ec
Mell <141835304+mellkarine@users.noreply.github.com>
2024-06-27 16:55:42 -0300

    Firts Uptade index.php
    Primeiro desenvolvimento do código no index.php, modificado para incluir o conteúdo principal

--- mell_codes/index.php
+++ mell_codes/index.php
@@ -1,5 +1,25 @@
 <?php
 
+    session_start();
 
+    include 'includes/db.php';
+    include 'includes/header.php';
+
+    // Para acessar uma URL (página) específica caso solicitada pelo usuário
+    if (isset($_GET['page'])) { // Código sendo usado para carregar o conteúdo da página solicitada 
+        $page = $_GET['page']; // Parâmetro da URL
+        $filepath = "pages/$page.php"; // Construção e "encaminhamento" da URL
+
+        if (file_exists($filepath)) {
+            include $filepath;
+        } else {
+            echo "<p> Página não encontrada </p>";
+        } 
+        
+    } else {
+        include 'pages/feed.php'; // Direcionamento para página principal caso nenhuma outra seja solicitada
+    }
+    
+    include 'includes/footer.php';
 
 ?>
